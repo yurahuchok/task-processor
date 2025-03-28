752bc454effb1b3cab455e1dc48b8bf79a5bdbab
Yuri Huchok <yuri.huchok.dev@gmail.com>
2025-03-29 00:53:29 +0100

    feat: publish messages to the queue

--- src/handler/publish.ts
+++ src/handler/publish.ts
@@ -3,11 +3,18 @@ import { APIGatewayProxyEventV2 } from "aws-lambda";
 import { getValidatedInput } from "../util/getValidatedInput";
 import { ZodError } from "zod";
 import { getConfig } from "../util/getConfig";
+import { SendMessageCommand, SQSClient } from "@aws-sdk/client-sqs";
 
 export async function handler(event: APIGatewayProxyEventV2) {
   try {
     const input = getValidatedInput(event, PublishRequest);
     const config = getConfig();
+    const client = new SQSClient();
+
+    await client.send(new SendMessageCommand({
+      QueueUrl: config.QUEUE_URL,
+      MessageBody: JSON.stringify(input),
+    }));
 
     return {
       statusCode: 200,
